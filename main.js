3995333350593a21720c0e1536d86a15b5d49506
Aadit <74043270+Aait900@users.noreply.github.com>
2021-11-09 20:23:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 function setup() {
-    var canvas=createCanvas(500, 500);
-    canvas.position(500,150);
-}
+    video = createCapture(VIDEO);
+    video.size(650, 500);
+    video.position(30,150)
+    
+    canvas = createCanvas(550, 500);
+    canvas.position(700,150);
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+    }
+    
+    function modelLoaded() {
+        console.log('Posenet is initialized!');
+    }
+    
+    function draw() {
+        background('#969A97');
+    }
+    
+    function gotPoses(results)
+    {
+        if(results.length > 0)
+        {
+            console.log(results);
+        }
+    }
+    
